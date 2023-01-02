6d8eb6ca2234aa837c6592ecf67a72333441da65
Mike Xu <mikexu1997@gmail.com>
2023-01-02 09:59:46 -0500

    Implemented search functionalities

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,13 @@ import "./App.css";
 import { Navbar } from "./layouts/NavBarAndFooter/Navbar";
 import { Home } from "./layouts/HomePage/Home";
 import { Footer } from "./layouts/NavBarAndFooter/Footer";
+import { SearchBooksPage } from "./layouts/SearchBookPage/SearchBooksPage";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <SearchBooksPage />
       <Home />
       <Footer />
     </div>
